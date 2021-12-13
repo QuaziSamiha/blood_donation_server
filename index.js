dd0cf5fcf7ceeec71f4030a8bb0d96f778405a51
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-12-13 22:35:24 +0600

    mongodb database connected

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+
 const app = express();
+const port = process.env.PORT || 5001;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.grbvc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+    const donorInfoCollection = client.db("Blood_Donation_CSE_3100").collection("DonorInfo");
+    console.log('database connected');
+    const info = {name: 'samiha', age: 22}
+    donorInfoCollection.insertOne(info)
+    .then(result = () => {
+        console.log('one data added');
+    })
+    // client.close();
+
+});
 
 app.get('/', (req, res) => {
-    res.send('thank uou for calling me');
+    res.send('Donate Blood, Save Life...Get Real Blessings!!!');
 })
 
-app.listen(5000, () => console.log('Listening to port 5000'));
+app.listen(5001, () => console.log('Listening to port 5001'));
