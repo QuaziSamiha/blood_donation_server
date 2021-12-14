8f256373c89c38472beaa05cd4037149a24177f9
QuaziSamiha <1703155@student.ruet.ac.bd>
2021-12-14 12:47:42 +0600

    donor data upload in database and show in ui part done

--- index.js
+++ index.js
@@ -4,31 +4,43 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
 
-
 const app = express();
-const port = process.env.PORT || 5001;
-
-
 app.use(cors());
 app.use(bodyParser.json());
+const port = process.env.PORT || 5001;
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.grbvc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-client.connect(err => {
-    const donorInfoCollection = client.db("Blood_Donation_CSE_3100").collection("DonorInfo");
-    console.log('database connected');
-    const info = {name: 'samiha', age: 22}
-    donorInfoCollection.insertOne(info)
-    .then(result = () => {
-        console.log('one data added');
-    })
-    // client.close();
-
-});
 
 app.get('/', (req, res) => {
     res.send('Donate Blood, Save Life...Get Real Blessings!!!');
 })
 
-app.listen(5001, () => console.log('Listening to port 5001'));
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.grbvc.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const donorInfoCollection = client.db("Blood_Donation_CSE_3100").collection("DonorInfo");
+
+    // uploading donor data in database
+    app.post('/beDonor', (req, res) => {
+        const newDonor = req.body;
+        donorInfoCollection.insertOne(newDonor)
+            .then(result => {
+                console.log('inserted count: ', result.insertedCount);
+                res.send(result.insertedCount > 0);
+            })
+    })
+
+    // loading donors data from database and display on search page
+    app.get('/searchDonors', (req, res) => {
+        console.log(req.query, req.query.city);
+        donorInfoCollection.find({
+            city: req.query.city,
+            bloodGroup: req.query.bloodGroup
+        })
+            .toArray((error, donors) => {
+                res.send(donors);
+            })
+    })
+});
+
+app.listen(5001, () => console.log(`Example app listening at http://localhost:${port}`));
